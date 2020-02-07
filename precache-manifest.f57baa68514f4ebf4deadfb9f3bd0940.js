03682f615aa55f921ec01b930d797ac7c603ec9b
Nagireddy <nagireddy.panditi@gmail.com>
2020-02-08 00:53:25 +0530

    Updates

---
+++ precache-manifest.f57baa68514f4ebf4deadfb9f3bd0940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "cd23450c59ffd8f53315",
+    "url": "/Weather-app/static/js/runtime~main.9b6b42b8.js"
+  },
+  {
+    "revision": "c7ee28906f98ab06906b",
+    "url": "/Weather-app/static/js/main.610aea02.chunk.js"
+  },
+  {
+    "revision": "6586e126983610623f9a",
+    "url": "/Weather-app/static/js/2.52ab1d9e.chunk.js"
+  },
+  {
+    "revision": "c7ee28906f98ab06906b",
+    "url": "/Weather-app/static/css/main.6584c911.chunk.css"
+  },
+  {
+    "revision": "6586e126983610623f9a",
+    "url": "/Weather-app/static/css/2.4f981819.chunk.css"
+  },
+  {
+    "revision": "d6b6fbdc59acef10360a01a5de41bee6",
+    "url": "/Weather-app/index.html"
+  }
+];
